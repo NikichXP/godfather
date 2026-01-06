3e7862c0834a7211062db5ba6cd78f26abb7cb1d
Nikita <nikichx2@gmail.com>
2026-01-06 17:16:52 +0100

    added actuator support

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,7 @@ extra["springCloudVersion"] = "2023.0.0"
 dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.cloud:spring-cloud-config-server")
 	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-server")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
